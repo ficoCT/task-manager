2fb448e01ce9b7a9b8a131a3b182691f3b056a81
FICO\rafal <rafalficek082@gmail.com>
2023-05-04 19:54:21 +0200

    Basic structure

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,26 @@
 const getAllTasks = (req, res) => {
-    res.send('all items from the file')}
+    res.send('getAllTasks')
+}
+
+const createTask = (req, res) => {
+    res.send('createTask')
+}
+
+const getTask = (req, res) => {
+    res.send('getTask')
+}
+const deleteTask = (req, res) => {
+    res.send('deleteTask')
+}
+const updateTask = (req, res) => {
+    res.send('updateTask')
+}
 
 module.exports = {
-  getAllTasks,
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
 
