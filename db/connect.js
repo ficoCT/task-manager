fa35d38e64d074fb169fe74d2a31f9807149659f
FICO\rafal <rafalficek082@gmail.com>
2023-05-15 21:02:05 +0200

    Env

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 
 const connectionString = 
-    'mongodb+srv://fico:ILq1fvzF8XwfOnKQ@projects.8gcu2tj.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://fico:B2Jp9PsAUlw4MD2a@projects.8gcu2tj.mongodb.net/products?retryWrites=true&w=majority'
 
-mongoose.connect(connectionString, {
-        useNewUrlParser: true
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
     })
-    .then(() => console.log('CONNECTED TO THE DB...'))
-    .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
